19b409e76ba960d65e0b65b4f72c3a84af29cf73
Kelsey Griffin <kjulegriffin@gmail.com>
2020-03-24 18:49:39 +0000

    use this.value.length to automatically update char counter

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,3 +1,7 @@
-$(document).ready(function() {
+$().ready(function() {
   console.log("*~ready~*");
+  $('#tweet-text').keyup( function(e) {
+    const currentLength = this.value.length;
+    $(this).siblings(".counter")[0].value = 140 - currentLength;
+  })
 });
