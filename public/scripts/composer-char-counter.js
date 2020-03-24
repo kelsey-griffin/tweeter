1cf7b37f3719a3ffb539d13b06db45a56b3c7228
Kelsey Griffin <kjulegriffin@gmail.com>
2020-03-24 19:30:11 +0000

    composer-char-counter updates with each keyup and switches to red if negative

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -3,5 +3,10 @@ $().ready(function() {
   $('#tweet-text').keyup( function(e) {
     const currentLength = this.value.length;
     $(this).siblings(".counter")[0].value = 140 - currentLength;
+    if ($(this).siblings(".counter")[0].value < 0) {
+      $(this).siblings(".counter").addClass("negative");
+    } else {
+      $(this).siblings(".counter").removeClass("negative");
+    }
   })
 });
