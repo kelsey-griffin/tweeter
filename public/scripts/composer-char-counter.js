6477084d140be9f808aac508d21e07e616e83263
Kelsey Griffin <kjulegriffin@gmail.com>
2020-03-24 19:44:26 +0000

    eslint on composer-char-counter, added hover class to tweet button

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,12 +1,13 @@
 $().ready(function() {
   console.log("*~ready~*");
-  $('#tweet-text').keyup( function(e) {
+  $('#tweet-text').keyup(function() {
     const currentLength = this.value.length;
-    $(this).siblings(".counter")[0].value = 140 - currentLength;
-    if ($(this).siblings(".counter")[0].value < 0) {
+    const remainingChars = $(this).siblings(".counter")[0];
+    remainingChars.value = 140 - currentLength;
+    if (remainingChars.value < 0) {
       $(this).siblings(".counter").addClass("negative");
     } else {
       $(this).siblings(".counter").removeClass("negative");
     }
-  })
+  });
 });
