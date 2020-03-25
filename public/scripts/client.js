d3ea17b64ff7600f8be949a0ab48e2a78919fc17
Kelsey Griffin <kjulegriffin@gmail.com>
2020-03-25 20:32:32 +0000

    ajax post request on tweet button and loadTweets function

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,3 +1,4 @@
+
 $(document).ready(function() {
   const createTweetElement = data => {
     const { name, avatars, handle } = data.user;
@@ -6,75 +7,51 @@ $(document).ready(function() {
     const date = fullDate.getUTCMonth() + "/" + fullDate.getUTCDate() + "/" + fullDate.getFullYear();
     const $markup = `
     <article class= "tweet">
-      <header>
-        <div class="user-info">
-          <span class= "avatar"><img src= ${avatars}></span>
-          <span class= "name">${name}</span>
-        </div>
-        <span class= "handle">${handle}</span>
-      </header>
-      <p class="tweet-content">${text}</p>
-      <footer>
-        <span class= "date-created">Tweeted on: ${date}</span>
-        <span class="icons">&#127988 &#128257 &#128153</span>
-      </footer>
+    <header>
+    <div class="user-info">
+    <span class= "avatar"><img src= ${avatars}></span>
+    <span class= "name">${name}</span>
+    </div>
+    <span class= "handle">${handle}</span>
+    </header>
+    <p class="tweet-content">${text}</p>
+    <footer>
+    <span class= "date-created">Tweeted on: ${date}</span>
+    <span class="icons">&#127988 &#128257 &#128153</span>
+    </footer>
     </article>
     `;
     return $markup;
   }
-
+  
   const renderTweets = tweets => {
+    const listOfTweets = [];
     for (element of tweets) {
-      const $tweet = createTweetElement(element);
-      $("#tweet-container").append($tweet);
-      console.log(createTweetElement(element));
+      listOfTweets.push(createTweetElement(element));
     }
+    $("#tweet-container").append(listOfTweets.join(' '));
   }
-
-
-// Fake data taken from initial-tweets.json
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ]
-  // renderTweets(data);
-  // Test / driver code (temporary). Eventually will get this from the server.
-  const tweetData = {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
-        "handle": "@SirIsaac"
-      },
-    "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-    "created_at": 1461116232227
-}
-
-renderTweets(data);
-//   const $tweet = createTweetElement(tweetData);
-
-// // Test / driver code (temporary)
-//   console.log($tweet); // to see what it looks like
-//   $('#tweet-container').append($tweet);
-});
+  const loadTweets = () => {
+    $.ajax({
+      url: '/tweets/', 
+      method: 'GET',
+      dataType: 'json' 
+    })   
+    .then(response => {
+     renderTweets(response);
+    }) 
+  }
+  loadTweets();
+  });
+$("#new-tweet-form").on("submit", function(e) {
+  e.preventDefault();
+  $.ajax({
+    url: '/tweets/',
+    method: 'POST',
+    data: $(this).serialize()
+  })
+  .then(response => {
+    console.log("RESPONSE: ");
+    console.log(response);
+  })
+})
