e1e0312d00d33aa8613ab486f1e48d095c7ae405
Kelsey Griffin <kjulegriffin@gmail.com>
2020-03-25 18:38:10 +0000

    createTweetElement() and renderTweets() functions in progress. nothing rendering so far.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,22 +1,77 @@
-const createTweetElement = data => {
-  console.log(data)
-}
+$(document).ready(function() {
+  console.log("in client js")
+  const createTweetElement = data => {
+    const { name, avatars, handle } = data.user;
+    const $markup = `
+    <article class= "tweet">
+      <header>
+        <div class="user-info">
+          <span class= "avatar"><img src= ${avatars}></span>
+          <span class= "name">${name}</span>
+        </div>
+        <span class= "handle">${handle}</span>
+      </header>
+      <p class="tweet-content">${data.content}</p>
+      <footer>
+        <span class= "date-created">Tweeted on: ${Date(data.created_at)}</span>
+        <span class="icons">&#127988 &#128257 &#128153</span>
+      </footer>
+    </article>
+    `;
+    return $markup;
+  }
 
-// Test / driver code (temporary). Eventually will get this from the server.
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
+// const renderTweets = tweets => {
+//   for (element of tweets) {
+//     const $tweet = createTweetElement(element);
+//     $("#tweet-container").append($tweet);
+//     console.log(createTweetElement(element));
+//   }
+// }
+
+
+// Fake data taken from initial-tweets.json
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
       "handle": "@SirIsaac"
     },
-  "content": {
+    "content": {
       "text": "If I have seen further it is by standing on the shoulders of giants"
     },
-  "created_at": 1461116232227
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ]
+  // renderTweets(data);
+  // Test / driver code (temporary). Eventually will get this from the server.
+  const tweetData = {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png",
+        "handle": "@SirIsaac"
+      },
+    "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+    "created_at": 1461116232227
 }
 
-const $tweet = createTweetElement(tweetData);
+  const $tweet = createTweetElement(tweetData);
 
 // Test / driver code (temporary)
-console.log($tweet); // to see what it looks like
-$('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+  console.log($tweet); // to see what it looks like
+  $('#tweets-container').append($tweet);
+});
