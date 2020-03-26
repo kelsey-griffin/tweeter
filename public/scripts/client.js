8ce76ebf9c0ce58bc1466c681d71b183f7599039
Kelsey Griffin <kjulegriffin@gmail.com>
2020-03-26 01:38:46 +0000

    refactor error handling to use jquery and  error-message section

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,13 +1,12 @@
-
 $(document).ready(function() {
 
   const createTweetElement = data => {
 
-  const escape =  function(str) {
-    let p = document.createElement('p');
-    p.appendChild(document.createTextNode(str));
-    return p.innerHTML;
-  }
+    const escape =  function(str) {
+      let p = document.createElement('p');
+      p.appendChild(document.createTextNode(str));
+      return p.innerHTML;
+    }
 
     const { name, avatars, handle } = data.user;
     const text = data.content.text;
@@ -25,7 +24,7 @@ $(document).ready(function() {
         <p class="tweet-content">${escape(text)}</p>
         <footer>
           <span class= "date-created">Tweeted on: ${date}</span>
-          <span class="icons">&#127988 &#128257 &#128153</span>
+          <span class="icons">⚑ ⟲ ♥︎</span>
         </footer>
       </article>
       `;
@@ -54,23 +53,33 @@ $(document).ready(function() {
   loadTweets();
   $("#new-tweet-form").on("submit", function(e) {
     e.preventDefault();
-    if ($('#tweet-text').val()) {
-      if ($('#tweet-text').val().length <= 140) {
-        // console.log(loadTweets())
-        $.ajax({
-          url: '/tweets/',
-          type: 'POST',
-          data: $(this).serialize(),
-          complete: () => {
-            // console.log("success form")
-            loadTweets();
-          }
-        })
-      } else {
-        alert("You've lot a lot to say!\nTweet must be less than or equal to 140 characters.")
-      }
-    } else {
-      alert("Cat got your tongue?\nEmpty tweets are not posted.")
+    const checkTweetValidity = () => {
+      const length = $('#tweet-text').val().length;
+      console.log($('section.error-msg')[0].innerText)
+      $('#error-msg').removeClass('error').slideDown(300);
+      if (length > 140) {
+        $('#error-msg').addClass('error');
+        $('section.error-msg')[0].innerText = "You've lot a lot to say!\nTweet must be less than or equal to 140 characters.";
+        return false;
+      
+      } else if (length === 0) {
+        $('#error-msg').addClass('error');
+        $('section.error-msg')[0].innerText = "Cat got your tongue? Empty tweets are not posted.";
+        return false;
+      
+      } else return true;
+    }
+    console.log(checkTweetValidity())
+    if (checkTweetValidity()) {
+      $.ajax({
+        url: '/tweets/',
+        type: 'POST',
+        data: $(this).serialize(),
+        complete: () => {
+          // console.log("success form")
+          loadTweets();
+        }
+      })
     }
   })
 });
