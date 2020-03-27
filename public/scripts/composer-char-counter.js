13d94c319fe164cfbffd77aae4643df1cba9ddaa
Kelsey Griffin <kjulegriffin@gmail.com>
2020-03-27 02:36:18 +0000

    add comments and spacing for readability

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,16 +1,21 @@
 $().ready(function() {
+  
   //update output to show number of characters remaining in tweet
   $('#tweet-text').keyup(function() {
+    
     const currentLength = this.value.length;
     const remainingChars = $(this).siblings(".counter")[0];
+    
     remainingChars.value = 140 - currentLength;
+    
     if (remainingChars.value < 0) {
       $(this).siblings(".counter").addClass("negative");
     } else {
       $(this).siblings(".counter").removeClass("negative");
     }
-    //change height of the textarea to fit tweet
+
+    //change height of the textarea to fit tweet as user types
     $(this).height(0).height(this.scrollHeight);
+
   });
- 
 });
